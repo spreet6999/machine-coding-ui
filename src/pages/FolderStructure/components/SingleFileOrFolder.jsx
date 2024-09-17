49b76f1056e246571e49a72e4ceb51d0ffe3f2b1
Shagunpreet Singh <42646816+spreet6999@users.noreply.github.com>
2024-09-17 19:12:19 +0000

    ADD doubleClickFeature in SingleFileOrFolder Component

--- src/pages/FolderStructure/components/SingleFileOrFolder.jsx
+++ src/pages/FolderStructure/components/SingleFileOrFolder.jsx
@@ -9,15 +9,37 @@ import style from "../style.module.css";
 SingleFolderOrFile.propTypes = {
   explorerObj: PropTypes.object,
 };
+
 export default function SingleFolderOrFile({ explorerObj = {} }) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isSingleClicked, setIsSingleClicked] = useState(false);
+
+  //* Double Click Feature Flag
+  const enableDoubleClickFeature = true;
+  let timerId = null;
+  const handleClick = () => {
+    if (enableDoubleClickFeature) {
+      if (!isSingleClicked && !timerId) {
+        setIsSingleClicked(true);
+        timerId = setTimeout(() => {
+          setIsSingleClicked(false)
+        }, 300)
+      } else if (isSingleClicked) {
+        setIsExpanded(!isExpanded);
+        timerId = null;
+      }
+    } else setIsExpanded(!isExpanded);
+  }
 
   if (!explorerObj) return null;
   if (Object.keys(explorerObj).length === 0) return null;
   if (explorerObj?.isFolder)
     return (
       <article className={style["single-folder-or-file-container"]}>
-        <span onClick={() => setIsExpanded((prevState) => !prevState)}>
+        <span
+          // onClick={() => setIsExpanded((prevState) => !prevState)}
+          onClick={handleClick}
+        >
           🗂{"  "}
           {explorerObj?.name}
         </span>
